e1121e5d3f017c7f72cb61404313769abbb9474e
BasicCorruption <saytleader@outlook.com>
2022-06-01 18:06:46 -0400

    Update PlayerEvents.java

--- src/net/mlgland/privateserver/events/PlayerEvents.java
+++ src/net/mlgland/privateserver/events/PlayerEvents.java
@@ -5,12 +5,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
-import org.bukkit.event.player.PlayerEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerPreLoginEvent;
 
 import java.util.UUID;
 
@@ -19,15 +15,14 @@ public class PlayerEvents implements Listener {
     @EventHandler
     public static void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
         UUID playerUUID = event.getUniqueId();
-        System.out.println(playerUUID);
         OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerUUID);
         Player player = offlinePlayer.getPlayer();
-        System.out.println(player);
-        assert player != null;
+
         if (!player.hasPermission("privateserver.join")) {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED + "[PrivateServer] You do not have permission to join this server! Please contact a server administrator if you think this is incorrect.\n");
             System.out.println("[PrivateServer] [Log] Player " + player.getName() + " has been kicked for insufficient permissions");
         }
+
     }
 
 }
