ce5e919730b228a792a0475a726db6cda065a257
BasicCorruption <saytleader@outlook.com>
2022-06-07 18:05:28 -0400

    Update PlayerEvents.java

--- src/net/mlgland/privateserver/events/PlayerEvents.java
+++ src/net/mlgland/privateserver/events/PlayerEvents.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.UUID;
 
@@ -18,19 +19,40 @@ public class PlayerEvents implements Listener {
         OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerUUID);
         Player player = offlinePlayer.getPlayer();
 
-//        if (!player.hasPermission("privateserver.join")) {
-//            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED + "[PrivateServer] You do not have permission to join this server! Please contact a server administrator if you think this is incorrect.\n");
-//            System.out.println("[PrivateServer] [Log] Player " + player.getName() + " has been kicked for insufficient permissions");
-//        }
+        // if (!player.hasPermission("privateserver.join")) {
+        // event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED
+        // + "[PrivateServer] You do not have permission to join this server! Please
+        // contact a server administrator if you think this is incorrect.\n");
+        // System.out.println("[PrivateServer] [Log] Player " + player.getName() + " has
+        // been kicked for insufficient permissions");
+        // }
 
-        player.getEffectivePermissions().forEach(e -> {
-            Boolean perm = e.getAttachment().getPermissions().get("privateserver.join");
-            if (perm) {
-                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED + "[PrivateServer] You do not have permission to join this server! Please contact a server administrator if you think this is incorrect.\n");
-                System.out.println("[PrivateServer] [Log] Player " + offlinePlayer.getName() + " has been kicked for insufficient permissions");
-            }
-        });
+        // player.getEffectivePermissions().forEach(e -> {
+        // Boolean perm = e.getAttachment().getPermissions().get("privateserver.join");
+        // if (perm) {
+        // event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED
+        // + "[PrivateServer] You do not have permission to join this server! Please
+        // contact a server administrator if you think this is incorrect.\n");
+        // System.out.println("[PrivateServer] [Log] Player " + offlinePlayer.getName()
+        // + " has been kicked for insufficient permissions");
+        // }
+        // });
 
+    }
+
+    @EventHandler
+    public static void onPlayerJoin(PlayerJoinEvent event) {
+        // get the player's UUID
+        String uuid = event.getPlayer().getUniqueId().toString();
+        // now we need to get the player's permissions
+        Boolean hasPerm = false;
+        if (event.getPlayer().hasPermission("privateserver.join")) {
+            hasPerm = true;
+        }
+        if (!hasPerm) {
+            event.getPlayer().kickPlayer(ChatColor.RED
+                    + "[PrivateServer] You do not have permission to join this server! Please contact a server administrator if you think this is incorrect.\n");
+        }
     }
 
 }
